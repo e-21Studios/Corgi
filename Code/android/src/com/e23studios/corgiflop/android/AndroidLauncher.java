80f01fa9d160838ca8b8ae3d080f9d3132e28aa3
WillTaylor95 <will.s.taylor95@gmail.com>
2015-02-05 16:21:22 +0000

    Generated project files
    Made the project using libgdx.jar

---
+++ Code/android/src/com/e23studios/corgiflop/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.e23studios.corgiflop.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.e23studios.corgiflop.mainGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new mainGame(), config);
+	}
+}
