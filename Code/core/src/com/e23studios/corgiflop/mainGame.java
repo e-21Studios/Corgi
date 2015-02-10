98afbe38432979178873a022ee4512d499a4a8f9
WillTaylor95 <will.s.taylor95@gmail.com>
2015-02-10 22:51:41 +0000

    Added camera + new defaultCorgi.png

--- Code/core/src/com/e23studios/corgiflop/mainGame.java
+++ Code/core/src/com/e23studios/corgiflop/mainGame.java
@@ -3,17 +3,20 @@ package com.e23studios.corgiflop;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class mainGame extends ApplicationAdapter {
+    private OrthographicCamera camera;
     private SpriteBatch batch;
     private Corgi corgi;
 
 	
 	@Override
 	public void create () {
+        camera = new OrthographicCamera(1080, 1920);
         batch = new SpriteBatch();
         corgi = new Corgi();
 	}
@@ -23,6 +26,7 @@ public class mainGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        batch.setProjectionMatrix(camera.combined);
         batch.begin();
         corgi.getSprite().draw(batch);
         batch.end();
